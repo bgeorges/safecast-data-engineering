cbd46f9893e3e762269868bd069e78eae718d26f
Bruno Georges <bgeorges@users.noreply.github.com>
2023-10-30 17:14:25 +0100

    Updating to include all data element from Safecast payload

---
+++ safecast_measurements_to_csv/main.py
@@ -0,0 +1,14 @@
+from safecast_measurements import api_reader
+from safecast_measurements import csv_writer
+
+def main():
+    # Fetch data using api_reader
+    json_data = api_reader.fetch_data()
+
+    # Process the data, e.g., convert it to CSV
+    csv_writer.json_to_csv(json_data)
+    print(json_data)
+
+if __name__ == "__main__":
+    main()
+
--- safecast_measurements_to_csv/safecast_measurements/csv_writer.py
+++ safecast_measurements_to_csv/safecast_measurements/csv_writer.py
@@ -4,7 +4,7 @@ from config import Config
 def json_to_csv(json_data):
     csv_file = Config.get('csv_file_path')
     with open(csv_file, 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=["timestamp", "id", "value"])
+        writer = csv.DictWriter(file, fieldnames=['id', 'value', 'latitude', 'station_id', 'channel_id', 'device_id', 'measurement_import_id', 'longitude', 'height', 'unit', 'devicetype_id', 'captured_at', 'sensor_id', 'location_name', 'original_id', 'user_id'])
         writer.writeheader()
         for entry in json_data:
             writer.writerow(entry)
